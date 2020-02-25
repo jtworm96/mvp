a0d6974d3f2f4f92d3fae00ac7d39d3183dca357
Jordan Worm <jordan.worm96@gmail.com>
2020-02-25 12:32:10 -0600

    mvp

---
+++ server/Server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const db = require('../Database/db');
+const Port = 3000;
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname + './Dist')));
+
+
+//connections/queries
+
+
+app.listen(Port, () => {
+  console.log(`listening on port ${Port}`);
+});
